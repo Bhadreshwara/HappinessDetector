c78c6dbc562da29dd0d612b6ea32fa00b2b59ef5
Nims972 <nirmalypatel@outlook.com>
2020-10-30 00:23:06 +0530

    changed python file

--- Happiness/Happiness_detector.py
+++ Happiness/Happiness_detector.py
@@ -17,17 +17,23 @@ def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        fx=x
+        fy=y
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+        cv.putText(roi_color,"Face",(fx, fy), cv.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 1)
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
         for (ex, ey, ew, eh) in eyes:
             cv.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh), (0, 255, 0), 2)
+            cv.putText(roi_color,"eyes",(ex, ey), cv.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0), 1)
         smile = smile_cascade.detectMultiScale(roi_gray, 1.8, 25)
         for (sx, sy, sw, sh) in smile:
             cv.rectangle(roi_color,(sx, sy),(sx+sw, sy+sh), (255, 0, 255), 2)
+            cv.putText(roi_color,"smile",(sx, sy), cv.FONT_HERSHEY_SIMPLEX, 0.55, (255, 0, 255), 1)
         nose = nose_cascade.detectMultiScale(roi_gray, 1.8, 20)
         for (nx, ny, nw, nh) in nose:
             cv.rectangle(roi_color,(nx, ny),(nx+nw, ny+nh), (255, 0, 0), 2)
+            cv.putText(roi_color,"nose",(nx, ny), cv.FONT_HERSHEY_SIMPLEX, 0.55, (255, 0, 0), 1)
     return frame
 
 #Doing some Face Recognition with the Webcame
